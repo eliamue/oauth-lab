54a26407b44c69794cb3218b1aa73f0b62ed3c13
Derek Hudgins <hudgins.derek@gmail.com>
2021-08-11 10:36:24 -0700

    added posts and comments SQL

--- sql/setup.sql
+++ sql/setup.sql
@@ -1,6 +1,23 @@
 DROP TABLE IF EXISTS users CASCADE;
+DROP TABLE IF EXISTS posts CASCADE;
+DROP TABLE IF EXISTS comments CASCADE;
 
 CREATE TABLE users (
     github_username TEXT NOT NULL PRIMARY KEY,
     github_avatar_url TEXT NOT NULL
 );
+
+CREATE TABLE posts (
+    id BIGINT GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
+    photo_url TEXT NOT NULL,
+    caption TEXT,
+    tags TEXT[],
+    username TEXT NOT NULL REFERENCES users(github_username)
+);
+
+CREATE TABLE comments (
+     id BIGINT GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
+     comment_by TEXT NOT NULL REFERENCES users(github_username),
+     post BIGINT NOT NULL REFERENCES posts(id),
+     comment TEXT NOT NULL
+)
